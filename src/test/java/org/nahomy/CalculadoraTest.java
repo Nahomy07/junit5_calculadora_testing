f4c50df6f8709e50eccb7ff32a369e8b451e2a12
Nahomy07 <estradabermudesnaho7@gmail.com>
2024-10-02 19:52:39 -0600

    segundo comit

--- src/test/java/org/nahomy/CalculadoraTest.java
+++ src/test/java/org/nahomy/CalculadoraTest.java
@@ -8,14 +8,26 @@ class CalculadoraTest {
 
     @Test
     void sumar() {
+        Calculadora calculadora = new Calculadora();
+        double sumaActual = calculadora.restar(20.0,4.0);
+        double respuestaEsperada = 24.0;
+        assertEquals(respuestaEsperada,sumaActual);
     }
 
     @Test
     void restar() {
+        Calculadora calculadora = new Calculadora();
+        double restaActual = calculadora.restar(20.0,4.0);
+        double respuestaEsperada = 16.0;
+        assertEquals(respuestaEsperada,restaActual);
     }
 
     @Test
     void multiplicar() {
+        Calculadora calculadora = new Calculadora();
+        double multiplicacionActual = calculadora.multiplicar(20.0,4.0);
+        double respuestaEsperada = 80.0;
+        assertEquals(respuestaEsperada, multiplicacionActual);
     }
 
     @Test
@@ -25,4 +37,6 @@ class CalculadoraTest {
         double respuestaEsperada = 5.0;
         assertEquals(respuestaEsperada, divisionActual);
     }
+
+
 }
